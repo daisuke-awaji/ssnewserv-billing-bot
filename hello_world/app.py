d49b9c1a515066fc9022337478324d208767a97d
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2018-09-13 09:47:55 +0900

    日ごと、月ごとの課金金額をSlackに通知

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,25 +1,52 @@
+# coding:utf-8
 import json
+
+import boto3
+import logging
+import datetime
 
 import requests
 
+SLACK_CHANNEL = 'robots'
+# Slack Incoming Webhook URLを指定。
+CHANNEL_URL  = "https://hooks.slack.com/services/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"
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+cost_exploler = boto3.client('ce')
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    start_time = datetime.date.today() - datetime.timedelta(days=1)
+    end_time   = datetime.date.today()
 
-    Arguments:
-        event LambdaEvent -- Lambda Event received from Invoke API
-        context LambdaContext -- Lambda Context runtime methods and attributes
+    day_cost = get_cost(str(start_time), str(end_time))
+    day_cost_msg = '本日の課金金額: ' + day_cost + ' USD'
+    logger.info(day_cost_msg)
 
-    Returns:
-        dict -- {'statusCode': int, 'body': dict}
-    """
+    dt = datetime.datetime.utcnow()
+    first_day_of_the_month = dt.date() - datetime.timedelta(days=dt.day - 1)
+    monthly_cost = get_cost(str(first_day_of_the_month), str(end_time))
+    monthly_cost_msg = '今月の課金金額: ' + monthly_cost + ' USD'
+    logger.info(monthly_cost_msg)
 
-    ip = requests.get('http://checkip.amazonaws.com/')
+    requests.post(CHANNEL_URL, data=json.dumps({
+        'text': day_cost_msg + '\n' + monthly_cost_msg,  # 通知内容
+        'username': u'billing-bot'  # ユーザー名
+    }))
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            'message': 'hello world',
-            'location': ip.text.replace('\n', ''),
-        })
-    }
+
+def get_cost(start_time, end_time):
+    response = cost_exploler.get_cost_and_usage(
+        TimePeriod={
+            'Start': start_time,
+            'End': end_time
+        },
+        Granularity='MONTHLY',
+        Metrics=[
+            'UnblendedCost',
+        ]
+    )
+    logger.info(response)
+    results = response['ResultsByTime'][0]
+    return results['Total']['UnblendedCost']['Amount']
